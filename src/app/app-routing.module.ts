0f774dc32134c1da8291f87ef10d53a90a4586e8
Heiner Gomez <agheinerag@gmail.com>
2019-03-28 12:37:18 -0500

    Creacion de pagina introductoria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', redirectTo: 'introductory-page', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'introductory-page', loadChildren: './introductory-page/introductory-page.module#IntroductoryPagePageModule' },
 ];
 
 @NgModule({
---
+++ src/app/introductory-page/introductory-page.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-introductory-page',
+  templateUrl: './introductory-page.page.html',
+  styleUrls: ['./introductory-page.page.scss'],
+})
+export class IntroductoryPagePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
