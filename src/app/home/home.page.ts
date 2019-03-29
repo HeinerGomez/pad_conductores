de372adfa8a9b2ba74b8ca7b2d3c9a8894a33e77
Heiner Gomez <agheinerag@gmail.com>
2019-03-29 17:34:32 -0500

    Se agrega navegación del home al login y del login al home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  constructor(private router: Router) {
+
+  }
+
+  /**
+   * @description Tiene como objetivo controlar el evento de hacer click o tap en el boton de login
+   * @author Heiner Gómez <alejandro.gomez@grupooet.com>
+   * @date 2019-03-29
+   * @returns void
+   */
+  public handleBtnLogin(): void {
+    this.router.navigate(['/login']);
+  }
 
 }
