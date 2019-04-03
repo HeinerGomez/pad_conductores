513c8f60fa72d4a993e55e26ccffd05bcb69797c
Heiner Gomez <agheinerag@gmail.com>
2019-04-03 12:47:10 -0500

    mensaje de bienvenida una vez haga login automatico

--- src/app/offers/offers.page.ts
+++ src/app/offers/offers.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageDataService } from '../services/storage-data.service';
 import { UserInSession } from '../interfaces/own/userInSession.interf';
+import { ActivatedRoute } from '@angular/router';
+import { UtilitiesService } from '../services/utilities.service';
 
 @Component({
   selector: 'app-offers',
@@ -11,12 +13,21 @@ export class OffersPage implements OnInit {
   
   public inSession: any;
 
-  constructor(private storageDataService: StorageDataService) { 
+  constructor(private storageDataService: StorageDataService, private activatedRoute: ActivatedRoute, 
+              private utilitiesService: UtilitiesService) { 
+    console.log('Constructor de ofertas');
     this.storageDataService.getUserInSession().then( (user: UserInSession) => {
       this.inSession = user;
     });
   }
 
   ngOnInit() {
+    console.log('ngOnInit');
+    this.activatedRoute.params.subscribe( params => {
+      console.log(params);
+      if (params.hasOwnProperty('shouldSessionMessage')) {
+        this.utilitiesService.showSnackbar('Bienvenido de vuelta al PAD');
+      }
+    });
   }
 }
