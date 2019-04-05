b2649110e1104003e25830dd20f05e305cb611a2
Heiner Gomez <agheinerag@gmail.com>
2019-04-05 06:37:36 -0500

    Creacion de archivo de expresiones regulares

---
+++ src/app/regex/formRegex.ts
@@ -0,0 +1,6 @@
+export const FORMREGEX = {
+    idCardCC: '^[0-9]{7,15}$',
+    textWithSpaces: '^(\\S\\D[a-zA-Z]+)\\D+[a-zA-Z]+$',
+    cellPhone: '^([1-9]{1})([0-9]{9})$',
+    email: '[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'
+};
