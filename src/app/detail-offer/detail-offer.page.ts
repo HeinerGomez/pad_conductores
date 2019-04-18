372ddc1e5bf3b8c51351df1ac99dbc439cf82ba9
Heiner Gómez <agheinerag@gmail.com>
2019-04-18 16:27:20 -0500

    Se agrega alert informativo al momento de aceptar la oferta

--- src/app/detail-offer/detail-offer.page.ts
+++ src/app/detail-offer/detail-offer.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { UtilitiesService } from '../services/utilities.service';
 
 @Component({
   selector: 'app-detail-offer',
   templateUrl: './detail-offer.page.html',
   styleUrls: ['./detail-offer.page.scss'],
 })
-export class DetailOfferPage implements OnInit {
+export class DetailOfferPage {
 
-  constructor() { }
+  constructor(private utilitiesService: UtilitiesService) { }
 
-  ngOnInit() {
+  /**
+   * @description Tiene como objetivo manejar la logica asociada a hacer click en aceptar oferta
+   * @author Heiner Gómez <alejandro.gomez@grupooet.com>
+   * @date 2019-04-18
+   * @param void
+   * @returns void
+   */
+  public handleTapAcceptOffer(): void {
+    this.utilitiesService.showInfoAlert('Oferta Aplicada', 'Se ha aplicado a la oferta correctamente');
   }
 
 }
