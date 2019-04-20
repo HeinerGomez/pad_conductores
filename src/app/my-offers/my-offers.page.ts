d706f0a26f7fdf8cf4d92cee7f68b6acda24a58b
Heiner Gomez <agheinerag@gmail.com>
2019-04-20 16:03:01 -0500

    Se agrega disenio basico de la pantalla de mis ofertas

--- src/app/my-offers/my-offers.page.ts
+++ src/app/my-offers/my-offers.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { IonSegment } from '@ionic/angular';
 
 @Component({
   selector: 'app-my-offers',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyOffersPage implements OnInit {
 
+  @ViewChild(IonSegment) segment: IonSegment;
+
   constructor() { }
 
   ngOnInit() {
+    this.segment.value = 'applied';
+  }
+
+  /**
+   * @description Tiene como objetivo manejar los cambios de los segmentos o secciones de "mis ofertas aplicadas"
+   *              y "mis ofertas confirmadas"
+   * @author Heiner Gómez <alejandro.gomez@grupooet.com>
+   * @date 2019-04-18
+   * @param event
+   * @returns void
+   */
+  public handleSegmentChanges(event): void {
+    const segmentValue = event.detail.value;
+  }
+
+  /**
+   * @description Tiene como objetivo manejar el refresh cuando se desliza la pantalla hacia arriba
+   * @author Heiner Gómez <alejandro.gomez@grupooet.com>
+   * @date 2019-04-18
+   * @param event
+   * @returns void
+   */
+  public handleSlideDownRefresh(event): void {
+    setTimeout( () => {
+      event.target.complete();
+    }, 3000);
   }
 
 }
