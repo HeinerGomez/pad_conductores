32bae303765a585e762a7e8b5ee867d470e7b944
Heiner Gómez <agheinerag@gmail.com>
2019-10-15 11:28:52 -0500

    se eliminan recursos que no se necesitan

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,12 @@
 export const environment = {
   production: true,
-  URL_API: 'https://dev.intrared.net:9443/pad_backend/api',
-  URL_STORAGE: 'https://dev.intrared.net:9443/pad_backend/storage/',
+  // URL_API: 'https://dev.intrared.net:9443/pad_backend/api',
+  // URL_STORAGE: 'https://dev.intrared.net:9443/pad_backend/storage/'
+  URL_STORAGE: 'https://logitech.intrared.net/ap/pad/v2/backend/storage/',
+  URL_API: 'https://logitech.intrared.net/ap/pad/v2/backend/api',
+  // URL_API: 'http://f1d9ea5d.ngrok.io/api',
   APP_ID_ONE_SIGNAL: '2e061205-2c57-4e4f-a48c-3c658da95e23',
   SENDER_ID: '387410221556',
-  ANDROID_ID: '1:387410221556:android:776431ba54a9b13c9e81a1'
+  ANDROID_ID: '1:387410221556:android:776431ba54a9b13c9e81a1',
+  SCA_URL: 'https://logitech.intrared.net/ap/sca/v1/backend/api/v1',
 };
