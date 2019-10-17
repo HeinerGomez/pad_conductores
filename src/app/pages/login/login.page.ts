99a4dd46d71a397dab004dd6a28abd058e30fd5d
Heiner Gómez <agheinerag@gmail.com>
2019-10-17 14:29:22 -0500

    se ajusta el boton de back del login, para que en cualquier caso redirigja al home

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -4,7 +4,7 @@ import { UserInSession } from '../../interfaces/own/userInSession.interf';
 import { AuthService } from '../../services/auth.service';
 import { AuthService as AuthSca } from '../../services/auth/auth.service';
 import { UtilitiesService } from '../../services/utilities.service';
-import { NavController, ModalController } from '@ionic/angular';
+import { NavController, ModalController, MenuController } from '@ionic/angular';
 import { first } from 'rxjs/operators';
 import { UserService } from 'src/app/services/api/user.service';
 import { ChangePasswordModalPage } from '../../modals/change-password-modal/change-password-modal.page';
@@ -24,11 +24,12 @@ export class LoginPage {
     private formBuilder: FormBuilder, private navController: NavController,
     private authService: AuthService, private utilitiesService: UtilitiesService,
     private _authService: AuthSca, private userService: UserService, 
-    private modalController: ModalController
+    private modalController: ModalController, private menuController: MenuController
   ) {
     this.inputTypePassword = 'password';
     this.iconTypePassword = 'eye';
     this.reactiveForm = this.buildReactiveForm();
+    this.menuController.enable(false);
   }
 
   ionViewDidEnter() {
@@ -84,23 +85,6 @@ export class LoginPage {
    * @returns void
    */
   public handleTapButtonLogin(): void {
-    // cargo de manera temporal un usuario ficticio
-    // const user: UserInSession = {
-    //   'username': 'Ero-sennin',
-    //   'realName': 'Heiner',
-    //   'firstLastName': 'Gómez',
-    //   'cellPhoneNumber': '3007045868'
-    // };
-    // this.authService.Authenticate(user).then( () => {
-    //   this.utilitiesService.showLoading('Iniciando sesión');
-    //   setTimeout( () => {
-    //     this.utilitiesService.closeLoading();
-    //     this.navController.navigateRoot('tab-offers');
-    //   }, 3000)
-    // }).catch(error => {
-    //   console.error('Error en handleTapButtonLogin: ', error);
-    //   this.utilitiesService.showSnackbar('Error al intentar iniciar sesión');
-    // });
     const { username, password } = this.reactiveForm.value;
     this.utilitiesService.showLoading('Iniciando sesión');
     this._authService.login(username, password).pipe(first()).subscribe((response: any) => {
@@ -119,6 +103,10 @@ export class LoginPage {
       }
     });
     modal.present();
+  }
+
+  public handleBackButton(): void {
+    this.navController.navigateRoot('home');
   }
 
 }
